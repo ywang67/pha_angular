2f8eede013a2fe493d80fb3eff96f20fcfda6a45
ywang67 <agentjob421@gmail.com>
2019-07-25 11:28:44 -0400

    dkfmksdk

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,33 +6,13 @@ import { Component, OnInit } from '@angular/core';
   'styleUrls': ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-  name: string;
-  count: string[];
+  username: string;
 
   constructor() {
-    console.log(this);
-    this.name = 'Paul';
-    this.count = [this.name];
-
-    this.addUserName = this.addUserName.bind(this);
+    this.username = 'Paul';
   }
 
   ngOnInit() {
 
-  }
-
-  addUserName(val: string): void {
-    if (this.count.indexOf(val) < 0
-      && val.length !== 0) {
-      this.count.push(val);
-    }
-  }
-
-  printChild(val: string): void {
-    console.log('from about: ', val);
-  }
-
-  onChangeType(evt: Event): void {
-    console.log((<HTMLInputElement>evt.target).value);
   }
 }
